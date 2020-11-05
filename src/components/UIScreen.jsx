0d4816836a4644e1d2fbd581a3c498f15513332f
BlazeOxin <nomaanakhtar643@gmail.com>
2020-11-05 20:59:32 +0500

    UI Styling start

--- src/components/UIScreen.jsx
+++ src/components/UIScreen.jsx
@@ -3,19 +3,19 @@ import "./UIScreen.css";
 
 const UIScreen = ({ onPlayRequest, onSettingsRequest, onQuitRequest }) => {
   return (
-    <div className="ui-screen">
+    <div className="ui-screen ">
       <div className="logo-title">
         <div className="logo">LOGO HERE</div>
         <h2>Chess</h2>
       </div>
       <div className="controls">
-        <button className="primary" onClick={onPlayRequest}>
+        <button className="b2 p-fh primary" onClick={onPlayRequest}>
           Play
         </button>
-        <button className="secondary" onClick={onSettingsRequest}>
+        <button className="b2 p-fh secondary" onClick={onSettingsRequest}>
           Settings
         </button>
-        <button className="danger" onClick={onQuitRequest}>
+        <button className="b2 p-fh danger" onClick={onQuitRequest}>
           Quit
         </button>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="main bg-darkblue">
+      <App />
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
