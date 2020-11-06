f7577ee8bd42963d2bd446e63fc19a819b33b0ca
NomaanAkhtar64 <nomaanakhtar644@gmail.com>
2020-11-06 23:43:57 +0500

    Pieces

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./GameBoard.css"
 import Piece from './Piece';
 
 const GameBoard = () => {
+    const [userPieces, setUserPieces] = useState([
+        { type: "king", posX: 3, posY: 0, variant: "black" },
+        { type: "queen", posX: 4, posY: 0, variant: "black" },
+        { type: "bishop", posX: 2, posY: 0, variant: "black" },
+        { type: "bishop", posX: 5, posY: 0, variant: "black" },
+        { type: "knight", posX: 1, posY: 0, variant: "black" },
+        { type: "knight", posX: 6, posY: 0, variant: "black" },
+        { type: "rook", posX: 0, posY: 0, variant: "black", },
+        { type: "rook", posX: 7, posY: 0, variant: "black" },
+        { type: "pawn", posY: 1, posX: 0, variant: "black" },
+        { type: "pawn", posY: 1, posX: 1, variant: "black" },
+        { type: "pawn", posY: 1, posX: 2, variant: "black" },
+        { type: "pawn", posY: 1, posX: 3, variant: "black" },
+        { type: "pawn", posY: 1, posX: 4, variant: "black" },
+        { type: "pawn", posY: 1, posX: 5, variant: "black" },
+        { type: "pawn", posY: 1, posX: 6, variant: "black" },
+        { type: "pawn", posY: 1, posX: 7, variant: "black" },
+    ]);
+
+    const [opponentPieces, setOpponentPieces] = useState([
+        { type: "king", posX: 3, posY: 0, variant: "white" },
+        { type: "queen", posX: 4, posY: 0, variant: "white" },
+        { type: "bishop", posX: 2, posY: 0, variant: "white" },
+        { type: "bishop", posX: 5, posY: 0, variant: "white" },
+        { type: "knight", posX: 1, posY: 0, variant: "white" },
+        { type: "knight", posX: 6, posY: 0, variant: "white" },
+        { type: "rook", posX: 0, posY: 0, variant: "white", },
+        { type: "rook", posX: 7, posY: 0, variant: "white" },
+        { type: "pawn", posY: 1, posX: 0, variant: "white" },
+        { type: "pawn", posY: 1, posX: 1, variant: "white" },
+        { type: "pawn", posY: 1, posX: 2, variant: "white" },
+        { type: "pawn", posY: 1, posX: 3, variant: "white" },
+        { type: "pawn", posY: 1, posX: 4, variant: "white" },
+        { type: "pawn", posY: 1, posX: 5, variant: "white" },
+        { type: "pawn", posY: 1, posX: 6, variant: "white" },
+        { type: "pawn", posY: 1, posX: 7, variant: "white" },
+    ]);
     return (
         <div className="game-board">
             <div className="squares">
@@ -88,7 +125,12 @@ const GameBoard = () => {
                 </div>
             </div>
             <>
-                <Piece type="queen" posY={0} posX={4} />
+                {userPieces.map((p, i) => (
+                    <Piece {...p} key={i} />
+                ))}
+                {opponentPieces.map((o, i) => (
+                    <Piece posY={7 - o.posY} posX={7 - o.posX} variant={o.variant} type={o.type} key={i} />
+                ))}
             </>
         </div>
     );
